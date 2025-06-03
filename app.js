acd5ca1e5d47e51d651711b9d9d578da71988914
norbeydanilo <norbey.danilo@outlook.com>
2025-06-02 22:50:19 -0500

    cambio mensaje say to

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js and Express!! Norbey :)');
+  res.send('Hello World from Node.js and Express!! Say hello to Norbey :)');
 });
 
 app.listen(PORT, () => {
